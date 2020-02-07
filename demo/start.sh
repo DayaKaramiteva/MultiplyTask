c1c2c3bb9d9c5584923b445a148c43069a1e084b
DayaKaramiteva <35263344+DayaKaramiteva@users.noreply.github.com>
2020-02-07 14:03:48 +0200

    Update start.sh

--- demo/start.sh
+++ demo/start.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 # execute ./start.sh from the root dir of the project
-
 logfile="./log/bootstrap.log"
-
 function catch_error () {
-    if [ $1 -ne 0 ]
-    then
+    if [ $1 -ne 0 ]; then
         printf "ERROR! Function "$2" failed. Please refer to $logfile for more information\n"
         exit 1
     fi
 }
-
 function create_log {
     if [[ ! -e $logfile ]]; then
         mkdir -p ./log
@@ -19,20 +15,19 @@ function create_log {
         :> "$logfile"
     fi
 }
-
 function build_maven {
     mvn clean install >> "$logfile" 2>&1
 }
-
 function container_ops {
     docker build -t dayanak/multi-task . >> "$logfile" 2>&1
 }
-
 function k8s_setup {
-    kubectl apply -f multitask.yaml >> "$logfile" 2>&1
-    kubectl expose deployment multi-task --type=LoadBalancer --port=8080 >> "$logfile" 2>&1
+    kubectl get deployment multi-task > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        kubectl apply -f multitask.yaml >> "$logfile" 2>&1
+        kubectl expose deployment multi-task --type=LoadBalancer --port=8080 >> "$logfile" 2>&1
+    fi
 }
-
 function run_app {
     printf "Truncating log file ...\n"
     create_log
